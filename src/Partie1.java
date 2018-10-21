5eed49fee170a1c0c3a24581bf8c925bf3811b84
Antoine Pichard <antoine.pichard28@hotmail.fr>
2018-10-21 15:38:01 +0200

    Add structure

---
+++ src/Partie1.java
@@ -0,0 +1,50 @@
+public interface Partie1{
+
+    /** initialise un plateau à partir d'un fichier texte
+     * @param fileName  le nom du fichier à lire
+     */
+    public void setFromFile(String fileName);
+
+    /** sauve la configuration de l'état courant (plateau et pièces restantes restantes) dans un fichier
+     * @param fileName  le nom du fichier à sauvegarder
+     * Le format doit être compatible avec celui utilisé pour la lecture
+     */
+    public void saveToFile(String fileName);
+
+    /** indique si le coup <choose> est valide pour le joueur <player> sur le plateau courant
+     * @param choose le choix de la pièce sous la forme "bgpr"
+     * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+     */
+    public boolean estchoixValide(String choose, String player);
+
+    /** indique si le coup <move> est valide pour le joueur <player> sur le plateau courant
+     * @param move le coup à jouer sous la forme "B2"
+     * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+     */
+    public boolean estmoveValide(String move, String player);
+
+    /** calcule les coups possibles pour le joueur <player> sur le plateau courant
+     * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+     */
+    public String[] mouvementsPossibles(String player);
+
+    /** calcule les choix possibles pour le joueur <player> sur les pièces restantes
+     * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+     */
+    public String[] choixPossibles(String player);
+
+    /** modifie le plateau en jouant le coup move avec la pièce choose
+     * @param move le coup à jouer sous la forme "B2"
+     * @param choose le choix de la pièce sous la forme "bgpr"
+     * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+     */
+    public void play(String move, String choose, String player);
+
+    /** modifie les pièces restantes en choisissant la pièce choose
+     * @param move le coup à jouer sous la forme "B2"
+     * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+     */
+
+    public boolean finDePartie();
+
+}
---
+++ src/PlateauQuartoPlus.java
@@ -0,0 +1,57 @@
+
+public class PlateauQuartoPlus implements Partie1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFromFile(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveToFile(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean estchoixValide(String choose, String player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estmoveValide(String move, String player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String[] mouvementsPossibles(String player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] choixPossibles(String player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void play(String move, String choose, String player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean finDePartie() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
